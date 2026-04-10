14fca5a37ec3edab8a752b1e128d444a2da6b89b
dr-musa-bala <bilalymedicals@gmail.com>
2026-04-10 19:46:29 +0300

    Feat: Adjust Dockerfile, app.py, requirement.txt

--- projects/08-health-api-jenkins/app.py
+++ projects/08-health-api-jenkins/app.py
@@ -1,20 +1,20 @@
-from flask import Flask
-from datetime import datetime
+from flask import Flask, jsonify
 import os
+from datetime import datetime
+from prometheus_flask_exporter import PrometheusMetrics  # New library!
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)  # This automatically tracks request numbers and latency
 
 @app.route('/health')
-def health_check():
-    # Get environment variable (default if not set)
-    location = os.getenv("APP_LOCATION", "Unknown-Location")
-    
-    return {
+def health():
+    return jsonify({
         "status": "UP",
-        "location": location,
+        "location": os.getenv("APP_LOCATION", "Kaduna-Axis-DC"),
         "system": "Kubernetes-WSL",
         "timestamp": datetime.now().isoformat()
-    }
+    })
 
 if __name__ == '__main__':
+    # Running on Port 80 as per our Phase 10 standards
     app.run(host='0.0.0.0', port=80)
